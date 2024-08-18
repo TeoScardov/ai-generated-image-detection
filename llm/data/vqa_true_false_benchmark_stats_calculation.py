691ff5916235dd70f19660ec56791ee4af2efc23
TeoScardov <matt.scardovi@gmail.com>
2024-08-18 09:13:51 +0000

    benchmark overhaul

---
+++ llm/data/vqa_true_false_benchmark_stats_calculation.py
@@ -0,0 +1,97 @@
+import os
+import argparse
+import json
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-r', '--results_file', default='./results.json', type=str)
+
+
+class CalculateMetrics:
+    def parse_pred_ans(self, pred_ans):
+        if pred_ans in ["True", "False"]:
+            return pred_ans
+        else:
+            return "other"
+
+    def compute_metric(self, gts, preds):
+        assert len(gts) == len(preds)
+
+        label_map = {
+            "True": 0,
+            "False": 1,
+            "other": 2,
+        }
+
+        gts = [label_map[x] for x in gts]
+        preds = [label_map[x] for x in preds]
+
+        acc = accuracy_score(gts, preds)
+
+        clean_gts = []
+        clean_preds = []
+        other_num = 0 
+        for gt, pred in zip(gts, preds):
+            if pred == 4:  # "other"
+                other_num += 1
+                continue
+            clean_gts.append(gt)
+            clean_preds.append(pred)
+        
+        conf_mat = confusion_matrix(clean_gts, clean_preds, labels=[0, 1])
+        precision = precision_score(clean_gts, clean_preds, average='macro')
+        recall = recall_score(clean_gts, clean_preds, average='macro')
+        
+        metric_dict = {
+            "acc": acc,
+            "precision": precision,
+            "recall": recall,
+            "confusion_matrix": conf_mat,
+            "other_num": other_num
+        }
+        
+        return metric_dict
+
+    def process_result(self, results_file):
+        with open(results_file, 'r') as f:
+            data = json.load(f)
+
+        gts = []
+        preds = []
+        task_name = "real/generated prediction"
+        task_other_ans_num = 0
+        img_num = len(data)
+
+        for item in data:
+            gt_ans = item["ground_truth"].strip()
+            pred_ans = item["answer"].strip()
+
+            assert gt_ans in ["True", "False"] 
+
+            pred_ans = self.parse_pred_ans(pred_ans)
+            assert pred_ans in ["True", "False", "other"]
+
+            gts.append(gt_ans)
+            preds.append(pred_ans)
+
+            if pred_ans == "other":
+                task_other_ans_num += 1
+
+        metric_dict = self.compute_metric(gts, preds)
+
+        task_score = metric_dict["acc"] * 100
+
+        print("total score:", task_score, "\n")
+        print("\t", task_name, " score:", task_score)
+        print("\tinvalid responses:", task_other_ans_num, "\n")
+        print("\tconfusion matrix:", metric_dict["confusion_matrix"], "\n")
+        print("\n")
+        
+        return 
+
+if __name__ == "__main__":
+    cal = CalculateMetrics()
+
+    args = parser.parse_args()
+    results_file = args.results_file
+    cal.process_result(results_file)
